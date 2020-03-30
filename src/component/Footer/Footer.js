50cac17d079beaa036c95794ad2dd2e977c9b7be
Nicolas Drouin <ballonposte@gmail.com>
2020-03-30 22:00:20 +0200

    Responsive Header and Footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -8,11 +8,10 @@ const Footer = () => {
                 <p>Danish wafer sweet roll donut powder cotton candy. Halvah liquorice I love cotton candy carrot cake sesame snaps I love. Halvah jujubes cake chupa chups danish.</p>
                 <hr/>
                 <ul>
-                    <li><a href='https://www.google.com'>Tart I love caramels</a></li>
                     <li><a href='https://www.google.com'>I love fruitcake icing</a></li>
+                    <li><a href='https://www.google.com'>Tart I love caramels</a></li>
                     <li><a href='https://www.google.com'>I love sugar plum halvah</a></li>
-                    <li><a href='https://www.google.com'>sugar plum lollipop</a></li>
-                    <li><a href='https://www.google.com'>I love pie I love</a></li>
+                    <li><a href='https://www.google.com'>Sugar plum lollipop</a></li>
                 </ul>
             </div>
         </footer>
