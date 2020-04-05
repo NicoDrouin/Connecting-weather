b547ee4b41080fae597fa586226be1a8c14c594c
Nicolas Drouin <ballonposte@gmail.com>
2020-04-05 22:24:59 +0200

    Fix css responsive helper

--- src/component/Weather/Forecast/ForecastWeek/ForecastWeek.tsx
+++ src/component/Weather/Forecast/ForecastWeek/ForecastWeek.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, Fragment } from 'react'
-import { useParams } from "react-router";
+import { useParams } from 'react-router'
 import './../forecast.scss'
 import './forecastWeek.scss'
 
